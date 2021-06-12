a253012d3e9ed7396af8f1a1152742a2661fe829
lfinkbiner <54183577+lfinkbiner@users.noreply.github.com>
2021-06-12 12:57:51 -0400

    added search form

--- src/App.js
+++ src/App.js
@@ -1,12 +1,17 @@
-
+import Dictionary from "./Dictionary";
 import './App.css';
 
 function App() {
   return (
     <div className="App">
+      <div className="container">
       <header className="App-header">
         Dictionary
       </header>
+      <main>
+        <Dictionary />
+      </main>
+      </div>
     </div>
   );
 }
