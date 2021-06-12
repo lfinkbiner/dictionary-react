6a33358b5e77c8fa5cd64c8ed6fb8ac566a8af7d
lfinkbiner <54183577+lfinkbiner@users.noreply.github.com>
2021-06-12 13:23:06 -0400

    added dictionary api

--- src/Dictionary.js
+++ src/Dictionary.js
@@ -1,12 +1,19 @@
 import React, {useState} from "react";
+import axios from "axios";
 import "./Dictionary.css";
 
 export default function Dictionary(){
     let [keyword, setKeyword] = useState(null);
 
+    function handleResponse (response){
+        console.log(response.data[0]);
+    }
     function search(event){
         event.preventDefault();
-        alert(`searching for ${keyword}`);
+
+        let apiUrl =`https://api.dictionaryapi.dev/api/v2/entries/en_US/${keyword}`;
+        console.log(apiUrl);
+        axios.get(apiUrl).then(handleResponse);
     }
 
     function handleKeywordChange(event){
