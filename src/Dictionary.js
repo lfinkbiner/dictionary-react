c0441fed91fb2f211f9488ae0a5535f920337125
lfinkbiner <54183577+lfinkbiner@users.noreply.github.com>
2021-06-14 20:57:47 -0400

    used api to create results, meanings

--- src/Dictionary.js
+++ src/Dictionary.js
@@ -1,12 +1,15 @@
 import React, {useState} from "react";
 import axios from "axios";
 import "./Dictionary.css";
+import Result from "./result";
 
 export default function Dictionary(){
     let [keyword, setKeyword] = useState(null);
+    let [results, setResults] =useState(null);
 
     function handleResponse (response){
-        console.log(response.data[0]);
+        console.log(response.data[0].meanings[0].definitions[0].definition);
+        setResults(response.data[0]);
     }
     function search(event){
         event.preventDefault();
@@ -24,5 +27,6 @@ export default function Dictionary(){
         <form onSubmit={search}>
             <input type="search" autoFocus="on" onChange={handleKeywordChange}/>
         </form>
+        <Result results={results} />
     </div>;
 }
