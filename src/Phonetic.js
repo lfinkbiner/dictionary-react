ba5a04ddab75d45ad6fcbd4ddd7f95d914677b62
lfinkbiner <54183577+lfinkbiner@users.noreply.github.com>
2021-06-17 13:17:21 -0400

    Update Phonetic.js

--- src/Phonetic.js
+++ src/Phonetic.js
@@ -3,9 +3,10 @@ import React from "react";
 export default function Phonetic(props){
     console.log(props.phonetic);
     return <div>
-        <a href={props.phonetic.audio} target ="_blank">
+        <a href={props.phonetic.audio}>
             Listen
         </a>
+        <br/>
         {props.phonetic.text}
     </div>;
 }
